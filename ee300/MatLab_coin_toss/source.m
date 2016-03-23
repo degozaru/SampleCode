baa89257aeb598f3c97f9d4dece06485afc9178f
Vincent Chan <chanv@rohan.sdsu.edu>
2016-03-22 18:28:10 -0700

    Update source.m

--- ee300/MatLab_coin_toss/source.m
+++ ee300/MatLab_coin_toss/source.m
@@ -4,7 +4,7 @@
 %flip those coins fam
 result = round(rand(10000,4));
 
-%Step 1: add the rows up and then plot them on a histogram
+%Step 1: PDF and CDF of sum of heads
 rowTotal = sum(result, 2);
 numHeads = zeros(5,2);
 %This the hand calculated PDF, as explained in the report
@@ -16,7 +16,12 @@ numHeads(5,2) = 10000 * (1/16);
 for i = 1:length(rowTotal)
     numHeads(rowTotal(i,1) + 1) = numHeads(rowTotal(i,1) + 1) + 1;
 end
-    
+%This is the CDF
+cdfSums = zeros(5,1);
+cdfSums(1) = numHeads(1,1)/10000;
+for i = 2:5
+    cdfSums(i) = cdfSums(i-1) + (numHeads(i,1)/10000);
+end
 
 %Step 2: Find run lengths
 runTotals = zeros(20,2);
@@ -39,14 +44,22 @@ for i=2:40000
         lastRes = result(i);
     end
 end
-%This will calculate the PDF of the estimated run length frequencies
+%This will calculate the PDF of the estimated PDF
 totalRuns = sum(runTotals,1);
 for i = 1:20
     runTotals(i,2) = totalRuns(1) * (1/(2.^i));
 end
+%This will calculate the CDF
+cdfRuns = zeros(5,1);
+cdfRuns(1) = 1/2;
+for i = 2:5
+    cdfRuns(i) = cdfRuns(i-1) + (1/(2.^i));
+end
+    
 
 
 %Step 3: Plot the figures
+%PDF
 figure
 
 %Graph 1 - 4 flips, number of heads
@@ -66,6 +79,24 @@ xlabel('Consecutive flips');
 ylabel('Frequency');
 legend('Experimental', 'Binominal Distribution');
 axis([0,10,0,11000]);
+
+%CDF
+figure
+
+    %Graph 3 - CDF of 4 flips
+    subplot(2,1,1);
+    bar(cdfSums);
+    title('Number of heads in 4 coin flips');
+    xlabel('Number of Heads');
+    ylabel('Total Probability');
+    set(gca,'XTickLabel',[0,1,2,3,4]);
+    
+    %Grpah 4 - CDF of consecutive flips
+    subplot(2,1,2);
+    bar(cdfRuns);
+    title('Number of consecutive flips (40k trials)');
+    xlabel('Consecutive flips');
+    ylabel('Total Probability');
 
 %The future is here,
 %It's just not widely distributed yet.
